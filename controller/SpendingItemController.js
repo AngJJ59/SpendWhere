1078311173e23bd38c97c1aa31de88c485c31a82
benderang26 <benderang26@gmail.com>
2023-07-08 15:58:06 +0800

    add delete, put, get single requests

--- controller/SpendingItemController.js
+++ controller/SpendingItemController.js
@@ -12,3 +12,44 @@ module.exports.createSpendingItem = (req, res) => {
 
     res.json(spendingItem)
 }
+
+module.exports.getAllSpendingItems = async (_req, res) => {
+    const spendingItems = await SpendingItem.find()
+
+    res.json(spendingItems)
+}
+
+module.exports.deleteSpendingItem = async (req, res) => {
+    const deleteItem = await SpendingItem.findByIdAndDelete(req.params.uniqueIdentifier)
+
+    res.json(deleteItem)
+}
+
+module.exports.getSpendingItem = async (req, res) => {
+    const spendingItem = await SpendingItem.findById(req.params.uniqueIdentifier)
+
+    res.json(spendingItem)
+}
+
+module.exports.updateSpendingItemInfo = async (req, res) => {
+    try {
+        updatedData = req.body
+
+        itemToUpdate = await SpendingItem.findByIdAndUpdate(
+            req.params.uniqueIdentifier, 
+            updatedData, {
+                new: true
+            }
+        )
+
+        if (!itemToUpdate) {
+            return res.status(404).json({ error: 'Spending item not found' });
+          }
+      
+          return res.status(200).json({ message: 'Spending item updated successfully', item: itemToUpdate });
+
+    } catch(err) {
+        console.log(err.message)
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+}
--- routes/spendingItemRoute.js
+++ routes/spendingItemRoute.js
@@ -1,4 +1,6 @@
 const {
+    updateSpendingItemInfo,
+    getSpendingItem,
     getAllSpendingItems,
     deleteSpendingItem,
     createSpendingItem
@@ -7,5 +9,9 @@ const {
 const router = require('express').Router()
 
 router.post('/spendingItem/new', createSpendingItem)
+router.get('/allItems', getAllSpendingItems)
+router.delete('/spendingItem/delete/:uniqueIdentifier', deleteSpendingItem)
+router.get('/spendingItem/getSpendingItem/:uniqueIdentifier', getSpendingItem)
+router.put('/spendingItem/update/:uniqueIdentifier', updateSpendingItemInfo)
 
 module.exports = router
